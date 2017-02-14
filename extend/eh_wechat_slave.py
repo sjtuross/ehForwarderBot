fb5960621ea969949fd1ef80f4f8d6e90f19c78f
Roy Xiang <work@royxiang.me>
2017-02-14 10:04:57 +0800

    [EWS]: send gif as video (as a flag)

--- extend/eh_wechat_slave.py
+++ extend/eh_wechat_slave.py
@@ -1,7 +1,9 @@
 import itchat
 import logging
 import mimetypes
+import os
 
+from channel import MsgType
 from plugins.eh_wechat_slave import WeChatChannel
 
 
@@ -17,3 +19,15 @@ class WechatExChannel(WeChatChannel):
     def poll(self):
         self.reauth()
         super().poll()
+
+    def send_message(self, msg):
+        if self._flag('gif_as_video', False) and msg.type == MsgType.Image and msg.mime == 'image/gif':
+            path = msg.path.rsplit('.', 1)[0]
+            if os.path.isfile(path):
+                try:
+                    os.remove(msg.path)
+                except FileNotFoundError:
+                    pass
+                msg.type = MsgType.Video
+                msg.path = path
+        super().send_message(msg)
